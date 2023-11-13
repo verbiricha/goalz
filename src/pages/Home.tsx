5b652f67c685f8d75f8053ecb696b0c2c90a6a93
verbiricha <bandarra+gith@protonmail.com>
2023-11-13 17:01:34 +0100

    fix: copy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -131,29 +131,29 @@ export default function Home() {
         mt={showAnimation ? "-12em" : "0"}
         label="Lightning Fundraisers"
         title="Empower Dreams"
-        description="Fund causes and goals with lightning. Zap Goals fundraising is instantly available without any fees."
-        ctaText="Create a zap goal"
+        description="Fund causes and goals with lightning. Goalz fundraising is instantly available without any fees."
+        ctaText="Create a goal"
         ctaAction={createZapGoal}
       />
       <FeaturedGoals events={events} />
       {!isLoggedOut && (
         <Link href="/all" mt={-16}>
-          See all active zap goals
+          See all active goals
         </Link>
       )}
       <CallToAction
         label="Almost unreal"
         title="Zero fees. Multiple recipients. Instant availability. It's kind of amazing!"
-        description="There’s no party in the middle so all funds go directly to the recipients, as soon as you zap them. Zap goals uses the bitcoin lightning network to send payments anywhere in the world for near-zero cost. This makes it easy to send $1,000 or $0.01."
-        ctaText="Create a zap goal"
+        description="There’s no party in the middle so all funds go directly to the recipients, as soon as you zap them. Goalz uses the bitcoin lightning network to send payments anywhere in the world for near-zero cost. This makes it easy to send $1,000 or $0.01."
+        ctaText="Create a goal"
         ctaAction={createZapGoal}
       />
       <Features />
       <CallToAction
         label="Your dreams await"
-        title="Get the boost you need. Start a zap goal for yourself or someone you know."
-        description="Zap Goals is an open source project powered by bitcoin, lightning and the nostr protocol and made possible by the folks at OpenSats."
-        ctaText="Create a zap goal"
+        title="Get the boost you need. Start a goal for yourself or someone you know."
+        description="Goalz is an open source project powered by bitcoin, lightning and the nostr protocol and made possible by the folks at OpenSats."
+        ctaText="Create a goal"
         ctaAction={createZapGoal}
       />
     </Stack>
