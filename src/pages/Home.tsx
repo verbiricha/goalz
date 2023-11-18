34f0f0174274565d8ba4c76a63ed5dddb1a845ee
verbiricha <bandarra+gith@protonmail.com>
2023-11-18 11:59:20 +0100

    refactor: extract orbiting goals component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,10 @@
-import { useState, useEffect } from "react";
-import {
-  usePrefersReducedMotion,
-  useBreakpointValue,
-  Flex,
-  Box,
-  Stack,
-} from "@chakra-ui/react";
+import { Stack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { NDKEvent } from "@nostr-dev-kit/ndk";
-import { motion } from "framer-motion";
 
 import CallToAction from "@goalz/components/CallToAction";
+import OrbitingGoals from "@goalz/components/OrbitingGoals";
 import FeaturedGoals from "@goalz/components/FeaturedGoals";
 import Features from "@goalz/components/Features";
-import { GoalBubble } from "@goalz/components/Goal";
 import { NEW_GOAL } from "@goalz/routes";
 import { GOAL } from "@goalz/const";
 
@@ -21,93 +12,10 @@ import Link from "@ngine/components/Link";
 import useEvents from "@ngine/nostr/useEvents";
 import useSession from "@ngine/hooks/useSession";
 
-const MotionBox = motion(Box);
-
-interface OrbitingGoalsProps {
-  events: NDKEvent[];
-}
-
-interface OrbitingItem {
-  id: number;
-  event: NDKEvent;
-  angle: number;
-}
-
-function tendToZeroOnLowerHalf(value: number) {
-  const mapped = value < 0 ? 0 : value > 360 ? 360 : value;
-  const sinValue = Math.sin((mapped - 180) * (Math.PI / 180));
-  const mappedValue = (sinValue + 1) / 2;
-  return mappedValue;
-}
-
-function OrbitingGoals({ events }: OrbitingGoalsProps) {
-  const orbitRadius = useBreakpointValue(
-    {
-      base: 160,
-      sm: 210,
-      lg: 340,
-      xl: 420,
-    },
-    {
-      ssr: false,
-    },
-  );
-  const [orbitingElements, setOrbitingElements] = useState<OrbitingItem[]>([]);
-  const [isHovering, setIsHovering] = useState(false);
-
-  useEffect(() => {
-    const initialElements = events.map((event, index) => ({
-      id: index,
-      event,
-      angle: (index / events.length) * 360,
-    }));
-
-    setOrbitingElements(initialElements);
-  }, []);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (isHovering) return;
-      setOrbitingElements((prevElements) =>
-        prevElements.map((element) => ({
-          ...element,
-          angle: (element.angle + 1) % 360,
-        })),
-      );
-    }, 80);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return (
-    <Flex align="center" justify="center" position="relative" h="12em" w="100%">
-      {orbitingElements.map((element) => (
-        <MotionBox
-          key={element.id}
-          position="absolute"
-          initial={{ x: 0, y: 0 }}
-          animate={{
-            x: orbitRadius! * Math.cos((element.angle * Math.PI) / 180),
-            y: orbitRadius! * Math.sin((element.angle * Math.PI) / 180),
-          }}
-          transition={{ duration: 1 }}
-          onMouseEnter={() => setIsHovering(true)}
-          onMouseLeave={() => setIsHovering(false)}
-          zIndex="-1"
-          opacity={tendToZeroOnLowerHalf(element.angle)}
-        >
-          <GoalBubble event={element.event} />
-        </MotionBox>
-      ))}
-    </Flex>
-  );
-}
-
 export default function Home() {
   const navigate = useNavigate();
   const [session] = useSession();
   const isLoggedOut = session === null;
-  const prefersReducedMotion = usePrefersReducedMotion();
   const { events, eose } = useEvents({
     kinds: [GOAL],
     ids: [
@@ -118,7 +26,7 @@ export default function Home() {
       "060f4f06455ee0a87db48f7d5f23b532bcc133cea7dd3bc9f2a20226f1bf2705",
     ],
   });
-  const showAnimation = eose && !prefersReducedMotion;
+  const showAnimation = eose;
 
   function createZapGoal() {
     navigate(NEW_GOAL);
