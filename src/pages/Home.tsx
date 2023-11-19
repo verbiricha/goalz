9de2af293a64c52ad53b4031149cfc507d3229ba
verbiricha <bandarra+gith@protonmail.com>
2023-11-19 09:44:02 +0100

    fix: remove hardcoded goal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,14 +21,13 @@ export default function Home() {
   const [session] = useSession();
   const isLoggedOut = session === null;
   const { events: supporters } = useSupporters(HEYA_PUBKEY);
-  const { events, eose } = useEvents([
+  const { id, events, eose } = useEvents([
     {
       kinds: [GOAL],
       ids: [
         "ebe64e839a6f8391bdfd0d7d8950588f296e8e00eb271cda36e3d1af610e9732",
         "bd3b899997cd4ce115532a84eabe598bb7547cab8f44b06812b2306d64761096",
         "9b734bc67402c034857ec3f2ecd8e74d61d38f46505067c5e53986cf70a0c4f6",
-        "25ade1845b93c5cd47d7b2c3f1bc622ada3bada5b87a0fd3714fd5a089d868c4",
         "060f4f06455ee0a87db48f7d5f23b532bcc133cea7dd3bc9f2a20226f1bf2705",
       ],
     },
@@ -48,7 +47,7 @@ export default function Home() {
 
   return (
     <Stack align="center" spacing={20} mt={12}>
-      {showAnimation && <OrbitingGoals events={featuredGoals} />}
+      {showAnimation && <OrbitingGoals key={id} events={featuredGoals} />}
       <CallToAction
         mt={showAnimation ? "-12em" : "0"}
         label="Lightning Fundraisers"
