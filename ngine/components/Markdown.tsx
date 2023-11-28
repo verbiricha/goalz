8e0af009f7b90a80d781542a9308359ef653aea9
verbiricha <bandarra+gith@protonmail.com>
2023-11-28 10:23:59 +0100

    wip: note

--- ngine/components/Markdown.tsx
+++ ngine/components/Markdown.tsx
@@ -1,8 +1,81 @@
-import { Stack, StackProps } from "@chakra-ui/react";
+import { useCallback, ReactNode } from "react";
+import { Stack, StackProps, Image } from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 import Link from "./Link";
+
+// eslint-disable-next-line no-useless-escape
+const FileExtensionRegex = /\.([\w]+)$/i;
+
+interface HyperTextProps {
+  link: string;
+  children: ReactNode;
+}
+
+export function HyperText({ link, children, ...rest }: HyperTextProps) {
+  const render = useCallback(() => {
+    try {
+      const url = new URL(link);
+      const extension =
+        FileExtensionRegex.test(url.pathname.toLowerCase()) && RegExp.$1;
+      if (extension) {
+        switch (extension) {
+          case "gif":
+          case "jpg":
+          case "jpeg":
+          case "png":
+          case "bmp":
+          case "webp": {
+            return (
+              <Image
+                src={url.toString()}
+                alt={url.toString()}
+                maxH="420px"
+                width="100%"
+                my={4}
+                objectFit="contain"
+              />
+            );
+          }
+          case "wav":
+          case "mp3":
+          case "ogg": {
+            return <audio key={url.toString()} src={url.toString()} controls />;
+          }
+          case "mp4":
+          case "mov":
+          case "mkv":
+          case "avi":
+          case "m4v":
+          case "webm": {
+            return <video key={url.toString()} src={url.toString()} controls />;
+          }
+          default:
+            return (
+              <Link {...rest} href={url.toString()}>
+                {children || url.toString()}
+              </Link>
+            );
+        }
+      } else {
+        return (
+          <Link {...rest} href={link}>
+            {children || link}
+          </Link>
+        );
+      }
+    } catch (error) {
+      return (
+        <Link {...rest} href={link}>
+          {children || link}
+        </Link>
+      );
+    }
+  }, [link, children]);
+
+  return render();
+}
 
 interface MarkdownProps extends StackProps {
   content: string;
